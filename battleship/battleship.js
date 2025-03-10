23d3a4aae950defc7068e172d44540fef5fcc1ba
Edgar <mandericoprinceedgar@gmail.com>
2025-03-10 00:25:00 -0700

    Updates to BattleshipJS.

--- battleship/battleship.js
+++ battleship/battleship.js
@@ -1,3 +1,37 @@
-console.log("Battleship Game Started");
-window.prompt("Enter Guess")
+// Declaring Variable for Locations
+var location1 = 3;
+var location2 = 4;
+var location3 = 5;
 
+var guess;
+var hits = 0;
+var guesses = 0;  
+
+var isSunk = false;
+
+// Game Logic
+while (isSunk == false) {
+    guess = prompt("Ready!, Aim!, Fire! (Enter Number 0-6)");
+
+    if (guess < 0 || guess > 6) {
+        alert("Please Enter a Valid Cell Number!");
+    }else {
+        guesses = guesses + 1;
+
+        if (guess == location1 || guess == location2 || guess == location3) {
+            hits = hits + 1;
+            alert("Hit!");
+
+                if (hits == 3) {
+                    isSunk == true;
+                    alert("You Sank my Battleship🚀");
+                }
+        } else {
+            alert("Miss!");
+        }
+    }
+} 
+
+var stats = "You sank my battleship at: " + guesses + " guesses" + " And your accuracy is: " (3/guesses);
+
+alert(stats);
