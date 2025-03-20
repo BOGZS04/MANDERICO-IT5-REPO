d204c53d60eb9d59fa0cc6a7d24e24eafb518846
Edgar <mandericoprinceedgar@gmail.com>
2025-03-20 00:26:23 -0700

    BattleshipV2 & Functions

---
+++ Getting Functional/function.js
@@ -0,0 +1,44 @@
+// var dogName = "Potchi";
+// var dogWeight = 23;
+// if (dogWeight > 20) {
+//     console.log(dogName + " says Bowow");
+// } else {
+//     console.log(dogName + "says Woof");
+// }
+
+// var dogName = "Hughie";
+// var dogWeight = 10;
+// if (dogWeight > 20) {
+//     console.log(dogName + " says Bowow");
+// } else {
+//     console.log(dogName + " says Woof");
+// }
+
+// var dogName = "Yumi";
+// var dogWeight = 13;
+// if (dogWeight > 20) {
+//     console.log(dogName + " says Bowow");
+// } else {
+//     console.log(dogName + " says Woof");
+// }
+
+// var dogName = "Chloe";
+// var dogWeight = 21;
+// if (dogWeight > 20) {
+//     console.log(dogName + " says Bowow");
+// } else {
+//     console.log(dogName + " says Woof");
+// }
+
+function bark(name, weight) {
+    if (weight > 20) {
+        console.log(name + " says Bowow");
+    } else {
+        console.log(name + " says Woof");
+    }
+}
+
+bark("Potchi", 23);
+bark("Yumi", 15);
+bark("Hughie", 12);
+bark("Chloe", 25);
