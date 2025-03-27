905e19850ce8efad93df9b5f44872e225d5ba56b
Edgar <mandericoprinceedgar@gmail.com>
2025-03-27 00:38:35 -0700

    Added Practice Run for Getting Functional

---
+++ Getting Functional/Retun Function/main.js
@@ -0,0 +1,21 @@
+// function calculateArea(r) {
+//     var area;
+//     if ( r <= 0) {
+//         return 0;
+//      } else {
+//             area = Math.PI * r * r;
+//             return area;
+//         }
+//     }
+
+// var radius = 0;
+// var theArea =  calculateArea(radius)
+// console.log ("The area is:" + theArea)
+
+function printName(firstName, lastName) {
+    fullName = firstName + lastName;
+    return fullName
+} 
+
+printName('Edgar ', 'Manderico')
+console.log("My full name is: " + fullName)
